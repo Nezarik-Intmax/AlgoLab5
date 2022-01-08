30ad1b4c22b4db7cff6e424b494207c281a94e0c
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-01-08 16:04:07 +0500

    Final

--- AlgoLab5/MyForm.h
+++ AlgoLab5/MyForm.h
@@ -134,14 +134,17 @@ namespace AlgoLab5 {
 		}
 #pragma endregion
 		int** graph;
+		bool** edges;
 		int* nodes;
 		bool* flags;
 		array <String^>^ pathS;
-		int N = 5;
+		int N = 50;
 		int count = 0;
 		array<System::Windows::Forms::Button^>^ graphBtn;
+		array<Label^, 2>^ labels = gcnew array<Label^, 2>(60, 60);
 		private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e){
-			graph = new int* [N];
+			graph = new int*[N];
+			edges = new bool*[N];
 			nodes = new int[N];
 			flags = new bool[N];
 			pathS = gcnew array<String^>(N);
@@ -149,23 +152,12 @@ namespace AlgoLab5 {
 			for(int i = 0; i < N; i++){
 				flags[i] = false;
 				graph[i] = new int[N];
+				edges[i] = new bool[N];
 				nodes[i] = std::numeric_limits<int>::max();
-				for(int j = 0; j < N; j++){graph[i][j] = -1;}
-				//graphBtn[i] = gcnew array<System::Windows::Forms::Button^>(N);
-				/*for(int j = 0; j < N; j++){
-					graph[i][j] = 0;
-					System::Windows::Forms::Button^ Node = (gcnew System::Windows::Forms::Button());
-					Node->Location = System::Drawing::Point(16 + (j * 75), 220 + (i * 50));
-					Node->Name = L"Node" + i + "_" + j;
-					Node->Size = System::Drawing::Size(20, 20);
-					Node->TabIndex = i;
-					Node->Tag = j;
-					Node->Text = "0";
-					//Node->TextChanged += gcnew System::EventHandler(this, &MyForm::Node_TextChanged);
-					//Node->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
-					graphBtn[i][j] = Node;
-					this->Controls->Add(Node);
-				}*/
+				for(int j = 0; j < N; j++){
+					graph[i][j] = -1;
+					edges[i][j] = false;
+				}
 			}
 		}
 		private: System::Void MyForm_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e){
@@ -183,40 +175,76 @@ namespace AlgoLab5 {
 		}
 		private: System::Void textBox1_KeyUp(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e){
 			if(e->KeyCode == Keys::Enter){
-				Algorithm();
+				Algorithm(Convert::ToInt32(textBox1->Text));
+				ShowPath(Convert::ToInt32(textBox1->Text), Convert::ToInt32(textBox2->Text));
 			}
 		}
-		private: System::Void Algorithm(){
-			for(int i = 0; i < N; i++){
+		private: System::Void Algorithm(int start){
+			for(int i = 0; i < count; i++){
 				flags[i] = false;
 				nodes[i] = std::numeric_limits<int>::max();
 				pathS[i] = "";
 			}
-			nodes[0] = 0;
+			nodes[start] = 0;
 			int min, minI;
-			for(int i = 0; i < N; i++){
-				min = std::numeric_limits<int>::max(), minI = 0;
+			label2->Text = "";
+			do{
+				min = std::numeric_limits<int>::max(), minI = 10000;
 				for(int j = 0; j < count; j++){
 					if((!flags[j]) && (nodes[j] < min)){
 						min = nodes[j];
 						minI = j;
 					}
 				}
-				for(int j = 0; j < count; j++){
-					if(!flags[j]){
-						if((graph[minI][j] != -1) && ((graph[minI][j] + nodes[minI]) < nodes[j])){
-							nodes[j] = graph[minI][j] + nodes[minI];
-							pathS[j] += minI + "->";
+				if(minI!=10000){
+					for(int j = 0; j < count; j++){
+						if(!flags[j]){
+							if((graph[minI][j] != -1) && ((graph[minI][j] + nodes[minI]) < nodes[j])){
+								nodes[j] = graph[minI][j] + nodes[minI];
+								label2->Text += "В массив кратчайших путей к "+j+" записан "+nodes[j]+" из вершины "+minI+"\n";
+								/*edges[minI][j] = true;
+								edges[j][minI] = true;
+								this->Invalidate();
+								_sleep(1000);*/
+							}
 						}
 					}
 				}
 				flags[minI] = true;
+			}while(minI != 10000);
+		}
+		int k = 1;
+		int* ver = new int[N];
+		private: System::Void ShowPath(int start, int endO){
+			int end = endO;
+			ver[0] = end;
+			k = 1;
+			int weight = nodes[end];
+
+			Pen^ pr = gcnew Pen(Color::Red);
+			while(end != start){
+				for(int i = 0; i < count; i++){
+					if(graph[i][end] != -1){
+						int temp = weight - graph[i][end];
+						if(temp == nodes[i]){
+							weight = temp;
+							end = i;
+							ver[k] = i;
+							k++;
+						}
+					}
+				}
 			}
-			this->label2->Text = "";
-			for(int o = 0; o < N; o++){
-				pathS[o] += o;
-				this->label2->Text += "S+P = " + nodes[o] + " \n" + pathS[o] + "\n\n";
+			label2->Text += "\nПуть из "+start+" в "+endO+" = "+nodes[endO]+"\n";
+			for(int i = k - 1; i >= 0; i--){
+				this->label2->Text += ver[i];
+				if(i>0) this->label2->Text += "->";
 			}
+			this->Invalidate();
+			/*if(!edges[i][j])
+				e->Graphics->DrawLine(pb, Point(graphBtn[i]->Location.X+10, graphBtn[i]->Location.Y+10), Point(graphBtn[j]->Location.X+10, graphBtn[j]->Location.Y+10));
+			else*/
+			//delete[](ver);
 		}
 		Button^ tmpNode = nullptr;
 		public: System::Void CreateEdge(System::Object^ sender, System::EventArgs^ e){
@@ -227,9 +255,29 @@ namespace AlgoLab5 {
 				if(dialogForm->ShowDialog(this) == ::DialogResult::OK){
 					graph[tmpNode->TabIndex][((Button^)sender)->TabIndex] = Convert::ToInt32(dialogForm->textBoxD->Text);
 					graph[((Button^)sender)->TabIndex][tmpNode->TabIndex] = Convert::ToInt32(dialogForm->textBoxD->Text);
-					this->label1->Text = dialogForm->textBoxD->Text;
-				} else{
-					this->label1->Text = "Cancelled";
+					int x1, x2, y1, y2;
+					x1 = graphBtn[tmpNode->TabIndex]->Location.X + 10;
+					x2 = graphBtn[((Button^)sender)->TabIndex]->Location.X + 10;
+					y1 = graphBtn[tmpNode->TabIndex]->Location.Y + 10;
+					y2 = graphBtn[((Button^)sender)->TabIndex]->Location.Y + 10;
+
+					if(labels[((Button^)sender)->TabIndex, tmpNode->TabIndex] != nullptr){
+						labels[((Button^)sender)->TabIndex, tmpNode->TabIndex]->Text = dialogForm->textBoxD->Text;
+					}else{
+						Label^ label = gcnew Label();
+						label->Text = graph[tmpNode->TabIndex][((Button^)sender)->TabIndex].ToString();
+
+						label->AutoSize = true;
+						label->Location = System::Drawing::Point((x1 + x2) / 2, (y1 + y2) / 2);
+						label->Name = L"label" + tmpNode->TabIndex.ToString() + ((Button^)sender)->TabIndex.ToString();
+						label->Size = System::Drawing::Size(35, 13);
+						label->TabIndex = 1;
+						label->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+							static_cast<System::Byte>(204)));
+						this->Controls->Add(label);
+						labels[tmpNode->TabIndex, ((Button^)sender)->TabIndex] = label;
+						labels[((Button^)sender)->TabIndex, tmpNode->TabIndex] = label;
+					}
 				}
 
 				delete dialogForm;
@@ -239,23 +287,39 @@ namespace AlgoLab5 {
 			this->Invalidate();
 		}
 		private: System::Void MyForm_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e){
-			Pen^ p = gcnew Pen(Color::Black);
-			for(int i=0; i < N; i++){
-				for(int j=0; j < N; j++){
-					if(graph[i][j] != -1){
-						e->Graphics->DrawLine(p, Point(graphBtn[i]->Location.X+10, graphBtn[i]->Location.Y+10), Point(graphBtn[j]->Location.X+10, graphBtn[j]->Location.Y+10));
+			Pen^ pb = gcnew Pen(Color::Black);
+			Pen^ pr = gcnew Pen(Color::Red);
+			int x1, x2, y1, y2;
+			for(int i=0; i < count; i++){
+				for(int j=0; j < count; j++){
+					if((graph[i][j] != -1)&&(i!=j)){
+						x1 = graphBtn[i]->Location.X + 10;
+						x2 = graphBtn[j]->Location.X + 10;
+						y1 = graphBtn[i]->Location.Y + 10;
+						y2 = graphBtn[j]->Location.Y + 10;
+						/*if(!edges[i][j])
+							e->Graphics->DrawLine(pb, Point(graphBtn[i]->Location.X+10, graphBtn[i]->Location.Y+10), Point(graphBtn[j]->Location.X+10, graphBtn[j]->Location.Y+10));
+						else*/
+						e->Graphics->DrawLine(pb, Point(x1, y1), Point(x2, y2));
 					}
 				}
+			}
+			for(int i = k - 1; i > 0; i--){
+				x1 = graphBtn[ver[i]]->Location.X + 10;
+				x2 = graphBtn[ver[i - 1]]->Location.X + 10;
+				y1 = graphBtn[ver[i]]->Location.Y + 10;
+				y2 = graphBtn[ver[i - 1]]->Location.Y + 10;
+				e->Graphics->DrawLine(pr, x1, y1, x2, y2);
 			}
 		}
 		private: System::Void labelUpdate(){
 			label1->Text = "";
-			for(int i = 0; i < count; i++){
+			/*for(int i = 0; i < count; i++){
 				for(int j = 0; j < count; j++){
 					label1->Text += (graph[i][j]).ToString() + "  ";
 				}
 				label1->Text += "\n";
-			}
+			}*/
 		}
 	};
 }
